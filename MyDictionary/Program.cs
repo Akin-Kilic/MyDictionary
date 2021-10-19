349c822ea7a35f7bb0985b82224058e9ef227905
Akın KILIÇ <akin602000@gmail.com>
2021-10-19 21:47:51 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> isimler = new MyDictionary<int,string>();
+            isimler.Add(10,"Engin");
+            
+
+            isimler.Add(5,"Akın");
+            
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
